ec7b874c5ef40bb0c0276cb400e4d0f737dbd882
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-30 23:05:09 +0200

    chore(next): configure Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
