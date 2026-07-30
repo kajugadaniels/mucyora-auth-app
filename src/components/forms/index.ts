6b819743397e0c37f0b687c22668d43de5da88a7
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-30 23:09:06 +0200

    chore(exports): export forms components

---
+++ src/components/forms/index.ts
@@ -0,0 +1,8 @@
+export { CitizenLookupForm } from "./CitizenLookupForm";
+export { CreateAccountForm } from "./CreateAccountForm";
+export { EmailVerificationForm } from "./EmailVerificationForm";
+export { ForgotPasswordForm } from "./ForgotPasswordForm";
+export { IdentityDocumentForm } from "./IdentityDocumentForm";
+export { LiveCheckForm } from "./LiveCheckForm";
+export { LoginForm } from "./LoginForm";
+export { ResetPasswordForm } from "./ResetPasswordForm";
