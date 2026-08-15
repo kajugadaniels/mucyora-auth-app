eff502e45cc9ecedd0047528c79ecb27b49a1e96
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-15 17:01:59 +0200

    feat(auth): export the HTTP gateway and session helpers

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,4 +1,5 @@
 export type { AuthGateway } from "./AuthGateway";
-export * from "./auth.contracts";
 export * from "./auth.errors";
+export * from "./auth-session";
 export * from "./auth.types";
+export { authGateway, HttpAuthGateway } from "./HttpAuthGateway";
